3391670006aa7e6931ac56dfd476150aff6ef4a7
AmarShukla1 <spamfreemenow12345@gmail.com>
2021-09-30 12:27:03 +0530

    done till leap

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a,...val) {
+   for(let i=0;i<val.length;i++){
+       a=a.filter(function(ele){
+         if(typeof(ele)!=typeof(val[i])){return true;}  
+         return ele!=val[i];
+       });
+    
+   }
+   return a;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+  if((typeof(start)!='number')||(typeof(end)!='number')){return 'ERROR';}
+  if(start<0||end<0){return 'ERROR';}
+  if(start>end){
+    [start,end]=[end,start];
+  }
+  let res=(end*(end+1))/2;
+  let res1=(start*(start-1))/2;
+  return res-res1;
 };
 
 // Do not edit below this line
