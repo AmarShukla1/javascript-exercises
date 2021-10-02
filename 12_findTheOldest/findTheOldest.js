b9aba0a60da797f97ffd1b2f290fa8f04bd64d83
AmarShukla1 <spamfreemenow12345@gmail.com>
2021-10-02 17:40:46 +0530

    finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  for(let i=0;i<arr.length;i++){
+      if(!arr[i].yearOfDeath){arr[i].yearOfDeath=2021;}
+  }  
+ arr.sort(function(a,b){
+  
+  if((a.yearOfDeath-a.yearOfBirth)>(b.yearOfDeath-b.yearOfBirth)){
+      return -1;
+  }
+  else{
+      return 1;
+  }
+ });//this 1,-1 return is the best way warna c++ ki tarike kar rahe the to bug aa raha tha wasn't 
+ //correctly sorting itself.
+ 
+ return arr[0].name;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
